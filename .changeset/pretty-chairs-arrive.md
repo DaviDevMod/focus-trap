e90eb561031f31543babe4cda8616f4d4c5592b4
Davide <davidevmod@gmail.com>
2022-05-09 16:54:40 +0100

    Refixed `assistTabbing`
    A previous fix didn't finish the job.
    Also added a comment explaining the logic behind the function.

---
+++ .changeset/pretty-chairs-arrive.md
@@ -0,0 +1,8 @@
+---
+'use-simple-focus-trap': patch
+---
+
+Refixed `assistTabbing`
+
+A previous fix didn't finish the job.
+Also added a comment explaining the logic behind the function.
--- packages/use-simple-focus-trap/src/use-single-trap/single-utils.ts
+++ packages/use-simple-focus-trap/src/use-single-trap/single-utils.ts
@@ -170,10 +170,12 @@ export function assistTabbing(event: KeyboardEvent, trapRefs: React.MutableRefOb
     // or a radio input belonging to the same radio group `boundary` belongs to,
     (isRadioInput(target) && isRadioInput(boundary) && target.name === boundary.name) ||
     // or if it either precedes `topTabbable` or succeeds `bottomTabbable` (only one of the two is checked).
-    !!(comparison && boundary.compareDocumentPosition(target as Node) & comparison);
+    !!(comparison && edge!.compareDocumentPosition(target as Node) & comparison);
 
   if (shiftKey) {
     if (isAssistedTabbingRequired(firstTabbable, topTabbable, 3)) to = lastTabbable;
+    // Should check `firstZeroTabIndex && lastMaxPositiveTabIndex`, but if there are no positive tab indexes
+    // `firstZeroTabIndex` is also `firstTabbable` and the above `if` is entered.
     else if (firstZeroTabIndex && isAssistedTabbingRequired(firstZeroTabIndex)) to = lastMaxPositiveTabIndex;
   } else {
     if (isAssistedTabbingRequired(lastTabbable, bottomTabbable, 5)) to = firstTabbable;
@@ -181,11 +183,86 @@ export function assistTabbing(event: KeyboardEvent, trapRefs: React.MutableRefOb
   }
 
   if (to) {
-    // Force the focus to either:
+    // Force the focus to go either to:
     event.preventDefault();
     // the checked radio button belonging to the same group `to` belongs to,
     if (isRadioInput(to) && !to.checked) {
       (document.querySelector('input[name=' + to.name + ']:checked') as FocusableElementRef)?.focus();
     } else to.focus(); // or simply `to`.
   }
-} // End of forceFocus();
+} // End of assistTabbing();
+
+/*
+
+`assistTabbing()` contains some logic that may deserve insights.
+
+I.
+Focus must always jump from `lastTabbable` to `firstTabbable`, and viceversa:
+
+  - When the focus is on `lastTabbable` and `TAB` is pressed, focus must go to `firstTabbable`.
+  - When the focus is on `firstTabbable` and `SHIFT + TAB` is pressed, focus must go to `lastTabbable`.
+
+II.
+Additionally, if the trap contains both elements with zero and with positive tab indexes,
+then focus must always jump also from `lastMaxPositiveTabIndex` to `firstZeroTabIndex`, and viceversa:
+
+  - When the focus is on `lastMaxPositiveTabIndex` and `TAB` is pressed, focus must go to `firstZeroTabIndex`.
+  - When the focus is on `firstZeroTabIndex` and `SHIFT + TAB` is pressed, focus must go to `lastMaxPositiveTabIndex`.
+
+  This is because for the browser the next tabbable element after one with positive tab index is either
+  the first element in the `document` (even ouside ot the trap) with an higher tab index (or equal, but following in order)
+  or the first element in the `document` (even ouside ot the trap) with zero tab index.
+
+  In theory positive tab indexes should be grouped by value (a group for "1", another for "2", etc)
+  and the focus should always be forced from the last element of a group to the first of another (and viceversa),
+  but in practive positive tab indexes are too rare (fairly, since they are an accessibility antipattern)
+  to make this logic worth to be implemented.
+  This logic differs from the currently implemented one, only in case the trap contains at least two elemnts
+  with positive tab indexes (say "1" and "2"), and there is at least one element outside of the trap with a tab index
+  whose value "T" is: 1 <= T <= 2.
+
+III.
+Assisted tabbing is always required when the focus is entering or leaving a radio group.
+
+  This is because when `firstTabbable`, `lastTabbable`, `lastMaxPositiveTabIndex`, `firstZeroTabIndex`
+  are to be set to an input being part of a radio group, they should be set to the currently chacked input,
+  but the hook actually set them to a fixed radio input acting as a representative for the whole group.
+
+  Here the choice is between the current approach, which consist in having logic to assist the tabbing from and to
+  inputs of a radio group, or the more intutive approach of using an event listener on the document which listens
+  for changes on the radio groups of the trap, to update the mentioned `trapRefs` with the currently checked radio input.
+
+  While the event listener approach removes the need to assist the tabbing for radio groups, it puts weight
+  on every trap update (they are triggered by the mutation observer) as there would be the need to scan
+  the mentioned `trapRefs` for the presence of a radio group and eventually attach or remove event listeners.
+
+  The current approach puts weigth on the event handler for `TAB` key press.
+
+  Drawing conslusions:
+  - Trap updates happens in time windows that are already quite intense, not only because of the logic running in this hook,
+    but also because of the external logic that triggered a trap update.
+    Moving logic from these busy times to quieter `TAB` key presses is definitely a good idea.
+  - The event listener approach still weights on the user's experience even if
+    they don't use the keyboard to navigate through the page.
+
+  How does the current approach work?
+  Whenever `TAB` is pressed the `event.target` is checkd to know if it's part of a radio group containing also one of
+  `firstTabbable`, `lastTabbable`, `lastMaxPositiveTabIndex`, `firstZeroTabIndex` and in that case the tab would be assisted
+  as if `event.target` was the one of the four`trapRefs` in question.
+  Whenever the tabbing has to be assisted (eg, `TAB` is pressed with `event.target` being `lastTabbable`), a check will be done
+  on the destination of the assisted tabbing (eg, `firstTabbable`) to know if it's part of a radio group, and in that case
+  the focus would go to the checked radio input in the group (rather than to what the hook considers as `firstTabbable`).
+
+IV.
+Assisted tabbing may be required when `event.target` is outside of the "edges" of the tabbable elements in the trap.
+
+  If the trap contains an element with negative tab index, the user can focus it (by means other than the keyboard) and
+  then `TAB` (or `SHIFT + TAB`) away from it.
+  If such element comes before `topTabbable` or after `bottomTabbable`, in document order, assisted tabbing is required
+  to keep the focus within the trap (from before `topTabbable` with a `SHIFT + TAB` to `lastTabbable` and
+  from after `bottomTabbable` with a `TAB` to `firstTabbable`).
+
+  It's not possible to use eg, `firstTabbable` instead of `topTabbable` because there could be tabbable elements
+  in the trap that come before `firstTabbable` in document order and in that case assisting the tabbing would be a mistake.
+
+*/
