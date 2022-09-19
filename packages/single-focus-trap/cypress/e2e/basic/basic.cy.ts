6592c766adb9abcc9baa07a7d099fbd681ea8443
Davide <davidevmod@gmail.com>
2022-09-19 11:34:38 +0100

    refactor the "basic" test of single-focus-trap

--- packages/single-focus-trap/cypress/e2e/basic/basic.cy.ts
+++ packages/single-focus-trap/cypress/e2e/basic/basic.cy.ts
@@ -1,7 +1,5 @@
 /// <reference types="cypress" />
 
-const CORRECT_FORWARD_CYCLE = '0123401234';
-const CORRECT_BACKWARD_CYCLE = CORRECT_FORWARD_CYCLE.split('').reverse().join('');
 let possibleTabbables: HTMLCollectionOf<HTMLButtonElement>;
 
 context('Basic focus trap behaviour', () => {
@@ -13,24 +11,25 @@ context('Basic focus trap behaviour', () => {
     });
   });
 
-  describe('The focus should cycle within the trap following a specific order, due to tab index values', () => {
+  describe('The focus should cycle within the trap following a specific order, dictated by tab index values', () => {
     it('Should cycle forward', () => {
-      cy.wrap(possibleTabbables).each((element) => {
-        cy.getTabCycle(element).then((cycle) => {
-          expect(CORRECT_FORWARD_CYCLE).to.have.string(cycle);
-        });
-      });
+      cy.verifyTabCycle(possibleTabbables);
     });
 
     it('Should cycle backward', () => {
-      cy.wrap(possibleTabbables).each((element) => {
-        cy.getTabCycle(element, false).then((cycle) => {
-          expect(CORRECT_BACKWARD_CYCLE).to.have.string(cycle);
-        });
-      });
+      cy.verifyTabCycle(possibleTabbables, 'BACKWARD');
     });
-
-    // it(`The focus trap should aknowledge relevant changes in its tabbable elements;
-    // e.g.: new tabbable elements appearing in the trap or former tabbable elements becoming untabbable.`, () => {});
   });
+
+  // describe('The focus should cycle within the trap following a specific order, dictated by tab index values', () => {
+  //   it(`The focus trap should aknowledge relevant changes in its tabbable elements;
+  //   e.g.: new tabbable elements appearing in the trap or former tabbable elements becoming untabbable.`, () => {
+  //     cy.verifyTabCycle(possibleTabbables);
+  //     cy.verifyTabCycle(possibleTabbables, 'BACKWARD');
+
+  //     // remove elements
+
+  //     // verify again
+  //   });
+  // });
 });
--- packages/single-focus-trap/cypress/support/commands.ts
+++ packages/single-focus-trap/cypress/support/commands.ts
@@ -2,46 +2,67 @@
 
 export {};
 
+type Direction = 'FORWARD' | 'BACKWARD';
+
+const CORRECT_CYCLE = '01234';
+const DEFAULT_TEST_CYCLE_LENGTH = 4;
+
 declare global {
   namespace Cypress {
     interface Chainable {
-      getNextTabTitle: (forward: boolean) => Cypress.Chainable<string>;
+      getNextTabTitle: (direction?: Direction) => Cypress.Chainable<string>;
       getTabCycle: (
         from: JQuery<HTMLElement>,
-        forward?: boolean,
+        direction?: Direction,
         len?: number,
         firstCall?: boolean,
         cycle?: string
       ) => Cypress.Chainable<string>;
+      verifyTabCycle: (collection: HTMLCollectionOf<HTMLButtonElement>, direction?: Direction, len?: number) => void;
     }
   }
 }
 
-Cypress.Commands.add('getNextTabTitle', (forward) => {
+Cypress.Commands.add('getNextTabTitle', (direction = 'FORWARD') => {
   cy.focused().then((from) => {
     const keysPressed = ['Tab'];
-    if (!forward) keysPressed.unshift('Shift');
+    if (direction === 'BACKWARD') keysPressed.unshift('Shift');
 
-    // Module '"cypress-real-events/commands/realPress"' declares 'KeyOrShortcut' locally, but it is not exported.
+    // Why `as any`? cause '"cypress-real-events/commands/realPress"' declares 'KeyOrShortcut' locally, but it is not exported.
     cy.realPress(keysPressed as any);
 
     cy.focused().then((to) => {
       const fromTitle = from.get(0).title;
       const toTitle = to.get(0).title;
 
-      if (fromTitle.startsWith('-')) expect(fromTitle.charAt(Number(forward) * -2 + 3)).to.equal(toTitle);
-      // else expect(Number(fromTitle) + Number(forward) * 2 - 1 === Number(toTitle));
-      // unnecessary cause the `cycle` would not match the `CORRECT_CYCLE` and the test would fail anyway.
+      // Check that elements outside the trap "pass the focus" to the right elements inside the trap.
+      if (fromTitle.startsWith('-')) expect(fromTitle.charAt(direction === 'FORWARD' ? 1 : 3)).to.equal(toTitle);
 
       return toTitle;
     });
   });
 });
 
-Cypress.Commands.add('getTabCycle', (from, forward = true, len = 4, firstCall = true, cycle = '') => {
+Cypress.Commands.add('getTabCycle', (from, direction = 'FORWARD', len = 0, firstCall = true, cycle = '') => {
   if (len <= 0) throw new Error('Please provide a positive length for the tab cycle.');
   if (firstCall) cy.wrap(from).focus(); // `if (firstCall && !from) { Cypress throws an error };`
-  cy.getNextTabTitle(forward).then((title) =>
-    len === 1 ? cycle + title : cy.getTabCycle(null, forward, len - 1, false, cycle + title)
+  cy.getNextTabTitle(direction).then((title) =>
+    len === 1 ? cycle + title : cy.getTabCycle(null, direction, len - 1, false, cycle + title)
   );
+});
+
+Cypress.Commands.add('verifyTabCycle', (collection, direction = 'FORWARD', len = DEFAULT_TEST_CYCLE_LENGTH) => {
+  const extendedCorrectCycle = {
+    FORWARD: CORRECT_CYCLE.repeat(Math.ceil(DEFAULT_TEST_CYCLE_LENGTH / CORRECT_CYCLE.length) + 1),
+    get BACKWARD() {
+      return this.FORWARD.split('').reverse().join('');
+    },
+  };
+
+  cy.wrap(collection).each((element) => {
+    cy.getTabCycle(element, direction, len).then((cycle) => {
+      expect(cycle).to.have.length(DEFAULT_TEST_CYCLE_LENGTH);
+      expect(extendedCorrectCycle[direction]).to.have.string(cycle);
+    });
+  });
 });
