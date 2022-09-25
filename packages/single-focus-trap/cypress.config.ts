20b8a650f27d681dd986acb94912880aa48da2d2
Davide <davidevmod@gmail.com>
2022-09-25 19:58:13 +0100

    e2e chores in single-focus-trap

--- packages/single-focus-trap/cypress.config.ts
+++ packages/single-focus-trap/cypress.config.ts
@@ -6,11 +6,17 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
 
+    // Setting an order to run the tests. The glob at the end ensures no test is left behind.
+    // Note that Cypress doesn't guarantee that the order in the `specPattern` array will be
+    // respected, but it currently is and should this behaviour change, no harm is done.
+    specPattern: ['cypress/e2e/tab-cycle.cy.ts', 'cypress/e2e/**/**.cy.ts'],
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
 
-      codeCoverageTask(on, config);
       // include any other plugin code...
+
+      codeCoverageTask(on, config);
 
       // It's IMPORTANT to return the config object
       // with any changed environment variables
