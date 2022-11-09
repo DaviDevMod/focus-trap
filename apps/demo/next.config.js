8737ce5ca0edf672cf9089b5ba754de397627daf
Davide <davidevmod@gmail.com>
2022-11-09 03:13:07 +0000

    Fix build errors

--- apps/demo/next.config.js
+++ apps/demo/next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: { esmExternals: true, externalDir: true },
+  typescript: {
+    // The demo has "exactOptionalPropertyTypes" and "noUncheckedIndexedAccess" enabled.
+    // https://github.com/microsoft/TypeScript/issues/41883
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
