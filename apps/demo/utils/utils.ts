26a8553e13bf51a28f4b1f2051366be7315db973
Davide <davidevmod@gmail.com>
2022-11-14 23:34:06 +0000

    (demo/utils.ts) Chore. TreWalker API?

--- apps/demo/utils/utils.ts
+++ apps/demo/utils/utils.ts
@@ -1,3 +1,6 @@
+// I just discovered the TreeWalker API: https://developer.mozilla.org/en-US/docs/Web/API/TreeWalker
+// But it looks like an overkill. I'll stick with the simple recursion below and keep this comment
+// just in case the use case becomes complex enough to justify a TreWalker implementation.
 export const getHTMLElementFlatSubTree = (
   root: HTMLElement | undefined,
   filter = (el: HTMLElement): boolean => true
