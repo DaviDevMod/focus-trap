49def8fe37397273b3c9fd46604411ac54e3821d
Davide <davidevmod@gmail.com>
2023-04-30 14:35:23 +0100

    Fix vercel-deploy.js script
    {deploy}

--- apps/demo/vercel-deploy.js
+++ apps/demo/vercel-deploy.js
@@ -1,25 +1,33 @@
 #!/usr/bin/env node
 
 /*
-End a commit message with the `DEEPLOY_FLAG`
+End a commit message with the `DEPLOY_SIGNATURE`
 to trigger a deployment when the commit is pushed.
-
-NOTE:
-The below implementation will fail when overwriting previously pushed commits.
-However the `DEPLOY_FLAG` should be used only when pushing on the main branch
-and force-pushing on the main branch should be avoided.
 */
 
 const { spawnSync } = require('child_process');
 const { exit } = require('process');
 
-DEPLOY_FLAG = '{deploy}';
+// Return early for branches other than the main one.
+// https://vercel.com/guides/how-do-i-use-the-ignored-build-step-field-on-vercel#with-a-script
+if (process.env.VERCEL_ENV != 'production') exit(0);
+
+const COMMIT_SEPARATOR = '\t~:separateeetarapes:~\t';
+const DEPLOY_SIGNATURE = '{deploy}';
 
 // Git command with output as utf-8 encoded string
 const git = (args) => spawnSync('git', args, { encoding: 'utf-8' });
 
-const commitMessagesSinceLastPush = git(['log', 'origin/main..', '--pretty=%B'])
-  .stdout.split('\n')
-  .filter((e) => e);
+// Whether there's a commit with the `DEPLOY_SIGNATURE` since the last deployed commit.
+// https://github.com/vercel/vercel/discussions/7251#discussioncomment-4731588
+// TODO: look into how "..origin/HEAD" could be implemented (in place of the current "..HEAD").
+const isDeploymentWanted = git([
+  'log',
+  `${process.env.VERCEL_GIT_PREVIOUS_SHA}..HEAD`,
+  `--pretty=%B${COMMIT_SEPARATOR}`,
+])
+  .stdout.split(COMMIT_SEPARATOR)
+  .some((message) => message.trimEnd().endsWith(DEPLOY_SIGNATURE));
 
-exit(Number(commitMessagesSinceLastPush.some((message) => message.endsWith(DEPLOY_FLAG))));
+// Let Vercel know whether a new deployment is wanted.
+exit(isDeploymentWanted);
