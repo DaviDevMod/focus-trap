edd0be48793fa50c1a47bd94a3c8fd279dd2a5bc
Davide <davidevmod@gmail.com>
2023-05-02 20:57:53 +0100

    Chores on vercel-deploy.js script

--- apps/demo/vercel-deploy.js
+++ apps/demo/vercel-deploy.js
@@ -12,35 +12,20 @@ const { exit } = require('process');
 // https://vercel.com/guides/how-do-i-use-the-ignored-build-step-field-on-vercel#with-a-script
 if (process.env.VERCEL_ENV != 'production') exit(0);
 
+const COMMITS_SEPARATOR = '\t~:separateeetarapes:~\t';
+const DEPLOY_SIGNATURE = '{deploy}';
+
 // Git command with output as utf-8 encoded string
 const git = (args) => spawnSync('git', args, { encoding: 'utf-8' });
-
-git(['remote', 'add', 'origin', 'https://github.com/DaviDevMod/focus-trap.git']);
-
-git(['fetch', 'origin', 'main']);
-
-git(['branch', '-u', 'origin/main']);
-
-console.log(`
-    git branch -vv: ${git(['branch', '-vv']).stdout}
-`);
-
-console.log(`
-    Error when trying to reference "origin":
-      ${git(['log', 'origin/HEAD']).stderr}
-`);
-
-const COMMIT_SEPARATOR = '\t~:separateeetarapes:~\t';
-const DEPLOY_SIGNATURE = '{deploy}';
 
 // Whether there's a commit with the `DEPLOY_SIGNATURE` since the last deployed commit.
 // https://github.com/vercel/vercel/discussions/7251#discussioncomment-4731588
 const isDeploymentWanted = git([
   'log',
-  `${process.env.VERCEL_GIT_PREVIOUS_SHA}..origin/HEAD`,
-  `--pretty=%B${COMMIT_SEPARATOR}`,
+  `${process.env.VERCEL_GIT_PREVIOUS_SHA}..HEAD`,
+  `--pretty=%B${COMMITS_SEPARATOR}`,
 ])
-  .stdout.split(COMMIT_SEPARATOR)
+  .stdout.split(COMMITS_SEPARATOR)
   .some((message) => message.trimEnd().endsWith(DEPLOY_SIGNATURE));
 
 // Let Vercel know whether a new deployment is wanted.
