2c5bd1c2ae4b7cf8a242f064d0da1555085c8b5a
Davide <davidevmod@gmail.com>
2023-05-07 23:07:19 +0100

    (focus-trap/src/global.d.ts) Remove custom type annotation for `Array.prototype.findLast`
    It is now buit-in since TypeScript v5.0 (https://github.com/microsoft/TypeScript/issues/48829#issuecomment-1442649966).

--- packages/focus-trap/src/global.d.ts
+++ packages/focus-trap/src/global.d.ts
@@ -5,9 +5,4 @@ declare global {
   interface Document {
     getElementById<T extends HTMLElement | SVGElement = HTMLElement>(elementId: string): T | null;
   }
-
-  // Support for `Array.prototype.findLast` ships with v5.0: https://github.com/microsoft/TypeScript/issues/48829
-  interface Array<T> {
-    findLast(predicate: (element: T, index: number, array: T[]) => unknown, thisArg?: any): T | undefined;
-  }
 }
