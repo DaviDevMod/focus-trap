341730df4341199475dc052c5076ca2413da5e99
Davide <davidevmod@gmail.com>
2023-05-19 11:45:33 +0100

    (demo/tailwind.config.js) Set `content`  to relative paths

--- apps/demo/tailwind.config.js
+++ apps/demo/tailwind.config.js
@@ -1,7 +1,10 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  // Always resolve paths relative to tailwind.config.js
+  // This will likely become the default behavior in the next major version of Tailwind.
+  // https://tailwindcss.com/docs/content-configuration#using-relative-paths
+  content: { relative: true, files: ['./src/**/*.{js,ts,jsx,tsx,mdx}'] },
   theme: {
     // screens: { xs: '525px', ...defaultTheme.screens },
     extend: {},
