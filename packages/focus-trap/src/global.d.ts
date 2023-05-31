97e88431728a2ed71f699f3d285d888e2d93d2df
Davide <davidevmod@gmail.com>
2023-05-31 20:01:59 +0100

    (focus-trap/src/global.d.ts) Widen up type of `getElementById`
    There is no need to be specific as the method call can always be<Casted>().

--- packages/focus-trap/src/global.d.ts
+++ packages/focus-trap/src/global.d.ts
@@ -3,6 +3,6 @@ export {};
 declare global {
   // See discussion: https://github.com/microsoft/TypeScript/issues/4689#issuecomment-690503791
   interface Document {
-    getElementById<T extends HTMLElement | SVGElement = HTMLElement>(elementId: string): T | null;
+    getElementById<T extends Element>(elementId: string): T | null;
   }
 }
