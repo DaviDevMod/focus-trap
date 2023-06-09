5a9e1d59c738efc6e6d462dfa935039876d08bfb
Davide <davidevmod@gmail.com>
2023-06-09 22:35:40 +0100

    Revert "(focus-trap/vite.config.js) Disable minification"
    This reverts commit 9fef6c395b4eed912dc515699e2b11f6054fc3a4.
    
    It makes more sense to minify, as there's nothing to tree-shake.

--- packages/focus-trap/vite.config.js
+++ packages/focus-trap/vite.config.js
@@ -5,7 +5,6 @@ import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   build: {
-    minify: false,
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'src/index.ts'),
