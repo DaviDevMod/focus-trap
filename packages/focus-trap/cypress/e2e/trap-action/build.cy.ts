80985b294c23840841d2696670ea78532a3ce2c5
Davide <davidevmod@gmail.com>
2023-06-11 13:49:51 +0100

    (focus-trap/cypress/e2e/trap-action/build.cy.ts) Chores

--- packages/focus-trap/cypress/e2e/trap-action/build.cy.ts
+++ packages/focus-trap/cypress/e2e/trap-action/build.cy.ts
@@ -3,13 +3,15 @@
 import { DEFAULT_ROOTS } from '../../support/commands';
 
 context('Test how the trap behaves after a "BUILD" action is performed.', () => {
-  // `check: true` is a stricter but slower way to verify that the trap is doing its job,
-  // checking that elements outside the trap pass the focus to the right elements inside the trap.
   describe('The focus should cycle within the trap following a specific order, dictated by document order and tab index values.', () => {
     beforeEach(() => {
       cy.visitDemoAndBuildTrap({ roots: DEFAULT_ROOTS, lock: false });
       cy.get('button[data-parent-id]').as('possibleTabbables');
     });
+
+    // `check === true` is a stricter but slower way to verify that the trap is doing its job,
+    // checking that elements outside the trap pass the focus to the right elements inside the trap.
+    // Note that `lock` has to be `false` in order for the check to be effective.
 
     it('Should cycle forward', () => {
       cy.get('@possibleTabbables').verifyTabCycle('FORWARD', true);
