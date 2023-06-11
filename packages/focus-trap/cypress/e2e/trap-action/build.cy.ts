e3b8a36d7fa6c60325d08a38ffd0c7c961d81d95
Davide <davidevmod@gmail.com>
2023-06-11 13:48:38 +0100

    (focus-trap/cypress/e2e/trap-action/build.cy.ts) Remove redundant test

--- packages/focus-trap/cypress/e2e/trap-action/build.cy.ts
+++ packages/focus-trap/cypress/e2e/trap-action/build.cy.ts
@@ -3,20 +3,6 @@
 import { DEFAULT_ROOTS } from '../../support/commands';
 
 context('Test how the trap behaves after a "BUILD" action is performed.', () => {
-  describe('Both a `trapConfig` and an array of `roots` are valid "BUILD" actions.', () => {
-    it('Should "BUILD" with a `trapConfig`.', () => {
-      cy.visitDemoAndBuildTrap({ roots: DEFAULT_ROOTS });
-
-      cy.get('button[data-parent-id]').verifyTabCycle();
-    });
-
-    it('Should "BUILD" with an array of `roots`.', () => {
-      cy.visitDemoAndBuildTrap(DEFAULT_ROOTS);
-
-      cy.get('button[data-parent-id]').verifyTabCycle();
-    });
-  });
-
   // `check: true` is a stricter but slower way to verify that the trap is doing its job,
   // checking that elements outside the trap pass the focus to the right elements inside the trap.
   describe('The focus should cycle within the trap following a specific order, dictated by document order and tab index values.', () => {
@@ -31,6 +17,14 @@ context('Test how the trap behaves after a "BUILD" action is performed.', () => 
 
     it('Should cycle backward', () => {
       cy.get('@possibleTabbables').verifyTabCycle('BACKWARD', true);
+    });
+  });
+
+  describe('An array of `roots` is valid "BUILD" action.', () => {
+    it('Should "BUILD" with an array of `roots`.', () => {
+      cy.visitDemoAndBuildTrap(DEFAULT_ROOTS);
+
+      cy.get('button[data-parent-id]').verifyTabCycle();
     });
   });
 });
