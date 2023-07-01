ec745026cf39c787112f4418da4c7545306b5b6f
Davide <davidevmod@gmail.com>
2023-07-01 13:43:52 +0100

    (demo/src/utils.ts) Chores

--- apps/demo/src/utils/utils.ts
+++ apps/demo/src/utils/utils.ts
@@ -1,21 +1,16 @@
 // The TreeWalker API (https://developer.mozilla.org/en-US/docs/Web/API/TreeWalker)
 // looks like an overkill for the current use case.
-export const getHTMLElementFlatSubTree = (
-  root: HTMLElement | undefined,
-  filter = (_el: HTMLElement): boolean => true
-) => {
+export function getHTMLElementFlatSubTree(root: HTMLElement, filter = (_el: HTMLElement): boolean => true) {
   const flatSubTree: HTMLElement[] = [];
 
-  const recursivelyPushInSubTree = (el: HTMLElement) => {
+  (function recursivelyPushInSubTree(el: HTMLElement) {
     flatSubTree.push(el);
     Array.from(el.children).forEach(
       (child) => child instanceof HTMLElement && filter(child) && recursivelyPushInSubTree(child)
     );
-  };
-
-  if (root) recursivelyPushInSubTree(root);
+  })(root);
 
   return flatSubTree;
-};
+}
 
 export const strToBoolOrItself = (str: string) => (/^(true|false)$/.test(str) ? str === 'true' : str);
